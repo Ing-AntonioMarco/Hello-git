f1dd143817793f3c86feea68309268317a6b54d9
Ing-AntonioMarco <ing.antoniomarco@gmail.com>
2025-11-03 12:48:37 -0400

    Agregue un par de lineas en el archivo JS y agregue estilos al menu en el archivo CSS

--- hellogit.js
+++ hellogit.js
@@ -1,4 +1,6 @@
 console.log("Hello, JavaScript!");
 console.log("This is a simple JavaScript example.");
 console.log("Enjoy coding!");
-console.log("Goodbye, JavaScript!");
+console.log("Goodbye, JavaScript!");    
+console.log("End of the script.");
+console.log("This is a simple JavaScript example.");
